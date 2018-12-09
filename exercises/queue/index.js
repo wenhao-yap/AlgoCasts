1760af7f01d2e2403c1ef7b41d7a72dba1fa8b43
wenhao-yap <yap.justin111@gmail.com>
2018-12-09 08:22:52 +0800

    queue and weave soln

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,18 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+  constructor(){
+    this.arr = [];
+  }
+
+  add(n){
+    this.arr.unshift(n);
+  }
+
+  remove(){
+    return this.arr.pop();
+  }
+}
 
 module.exports = Queue;
