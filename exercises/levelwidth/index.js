d38b433fc4d92cedc7d3f889593a95dcda12ef0a
Yap Wen Hao  NCS <wenhao.yap@ncs.com.sg>
2019-01-02 11:58:54 +0800

    finish levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,20 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) { //using BFS
+  const counters = [0];
+  const arr = [root,'s'];
+  while(arr.length > 1){
+    const node = arr.shift();
+    if(node === 's'){
+      arr.push('s');
+      counters.push(0);
+    } else {
+      arr.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+  return counters;
+}
 
 module.exports = levelWidth;
