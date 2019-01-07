798d65f8d805112750a1f706e7cd62cd980f369c
Yap Wen Hao  NCS <wenhao.yap@ncs.com.sg>
2019-01-07 17:36:34 +0800

    did bubblesort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,12 +1,23 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
+//BubbleSort works by repeatedly swapping the adjacent elements if they are in wrong order.
 function bubbleSort(arr) {
-
+  for(let i=0; i<arr.length-1;i++){
+    for(let j=0; j<arr.length-1; j++){
+      if(arr[j]>arr[j+1]){
+        const temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  return arr;
 }
 
+/* sorts by repeatedly finding min element (considering ascending order) from unsorted part 
+and putting it at the beginning. */
 function selectionSort(arr) {
-
 }
 
 function mergeSort(arr) {
