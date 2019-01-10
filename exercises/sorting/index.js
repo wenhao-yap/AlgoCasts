5e2d7de62812835cbc3aec96248c5fee5604dea5
Yap Wen Hao  NCS <wenhao.yap@ncs.com.sg>
2019-01-10 09:58:18 +0800

    added selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -18,10 +18,27 @@ function bubbleSort(arr) {
 /* sorts by repeatedly finding min element (considering ascending order) from unsorted part 
 and putting it at the beginning. */
 function selectionSort(arr) {
+  for(let i=0; i<arr.length;i++){
+    let minIdx = i;
+    
+    //get the lowest index of curr array
+    for(j = i+1; j<arr.length;j++){
+      if(arr[minIdx]>arr[j]){
+        minIdx = j;
+      }
+    }
+
+    if(minIdx != i){
+      const lowest = arr[minIdx];
+      arr[minIdx] = arr[i];
+      arr[i] = lowest;
+    }
+  }
+  return arr;
 }
 
 function mergeSort(arr) {
-
+  
 }
 
 function merge(left, right) {
